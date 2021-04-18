a17de390035913ecc2f97e7bae0a6314b9e99a74
maya <1327909321@qq.com>
2021-04-18 22:25:45 +0800

    4.18

--- manjaro/.config/fish/config.fish
+++ manjaro/.config/fish/config.fish
@@ -1,1 +1,8 @@
 if test -f /home/ice/.autojump/share/autojump/autojump.fish; . /home/ice/.autojump/share/autojump/autojump.fish; end
+
+begin
+    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
