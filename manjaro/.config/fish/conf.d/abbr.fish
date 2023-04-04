b5de941dfacd340ce690d9afdd3462c24a8851f8
mayapony <1327909321@qq.com>
2023-04-04 13:33:44 +0800

    ✨ update config

--- manjaro/.config/fish/conf.d/abbr.fish
+++ manjaro/.config/fish/conf.d/abbr.fish
@@ -1,21 +1,21 @@
 #! /usr/bin/fish
-abbr -a -U -- s fastfetch
-abbr -a -U -- rm trash
-abbr -a -U -- p pnpm
-abbr -a -U -- ls 'colorls --dark'
-abbr -a -U -- ll 'colorls --dark -l'
-abbr -a -U -- sys 'sudo systemctl'
-abbr -a -U -- fzf-p fzf\ --preview\ \'bat\ --color=always\ --style=numbers\ --line-range=:500\ \{\}
-abbr -a -U -- tree 'exa --icons --tree'
-abbr -a -U -- c clear
-abbr -a -U -- ra ranger
-abbr -a -U -- lg lazygit
-abbr -a -U -- trash rm
-abbr -a -U -- vi nvim
-abbr -a -U -- la 'colorls --dark -a'
-abbr -a -U -- pac 'sudo pacman'
+abbr -a -- s fastfetch
+abbr -a -- rm trash
+abbr -a -- p pnpm
+abbr -a -- ls 'colorls --dark'
+abbr -a -- ll 'colorls --dark -l'
+abbr -a -- sys 'sudo systemctl'
+abbr -a -- fzf-p fzf\ --preview\ \'bat\ --color=always\ --style=numbers\ --line-range=:500\ \{\}
+abbr -a -- tree 'exa --icons --tree'
+abbr -a -- c clear
+abbr -a -- ra ranger
+abbr -a -- lg lazygit
+abbr -a -- trash rm
+abbr -a -- vi nvim
+abbr -a -- la 'colorls --dark -a'
+abbr -a -- pac 'sudo pacman'
 abbr -a -- ec 'emacsclient -nw'
-abbr -a -U -- pa pnpm add
-abbr -a -U -- pi pnpm install
-abbr -a -U -- pd pnpm add -D
-abbr -a -U -- d pnpm dev
+abbr -a -- pa pnpm add
+abbr -a -- pi pnpm install
+abbr -a -- pd pnpm add -D
+abbr -a -- d pnpm dev
