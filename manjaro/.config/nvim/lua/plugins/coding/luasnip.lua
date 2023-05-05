7a91ec0a169390a3427873dd09c4d08147a922bc
mayapony <1327909321@qq.com>
2023-05-05 22:35:50 +0800

    📝 update doc...

--- manjaro/.config/nvim/lua/plugins/coding/luasnip.lua
+++ manjaro/.config/nvim/lua/plugins/coding/luasnip.lua
@@ -14,6 +14,28 @@ return {
     local luasnip = require("luasnip")
     luasnip.filetype_extend("javascript", { "javascriptreact" })
     luasnip.filetype_extend("javascript", { "html" })
+
+    -- forget the current snippet when leaving the insert mode
+    -- source: https://github.com/L3MON4D3/LuaSnip/issues/656
+    local unlinkgrp = vim.api.nvim_create_augroup(
+      'UnlinkSnippetOnModeChange',
+      { clear = true }
+    )
+
+    vim.api.nvim_create_autocmd('ModeChanged', {
+      group = unlinkgrp,
+      pattern = { 's:n', 'i:*' },
+      desc = 'Forget the current snippet when leaving the insert mode',
+      callback = function(evt)
+        if
+            luasnip.session
+            and luasnip.session.current_nodes[evt.buf]
+            and not luasnip.session.jump_active
+        then
+          luasnip.unlink_current()
+        end
+      end,
+    })
   end,
   opts = {
     history = true,
--- manjaro/.config/nvim/lua/plugins/ui/themes.lua
+++ manjaro/.config/nvim/lua/plugins/ui/themes.lua
@@ -1,26 +1,68 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   enabled = false,
-  --   config = function()
-  --     require("catppuccin").setup({
-  --       -- transparent_background = true,
-  --       no_italic = true
-  --     })
-  --   end
-  -- },
+  {
+    "2nthony/vitesse.nvim",
+    dependencies = {
+      "tjdevries/colorbuddy.nvim"
+    },
+    config = function()
+      require("vitesse").setup {
+        comment_italics = true,
+        transparnet_background = true,
+        transparent_float_background = true, -- aka pum(popup menu) background
+        reverse_visual = false,
+        dim_nc = false,
+        cmp_cmdline_disable_search_highlight_group = false, -- disable search highlight group for cmp item
+      }
+    end
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      require("catppuccin").setup {
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        term_colors = true,
+        transparent_background = false,
+        no_italic = true,
+        no_bold = false,
+        styles = {
+          comments = {},
+          conditionals = {},
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+        },
+        color_overrides = {
+          mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        },
+        highlight_overrides = {
+          mocha = function(C)
+            return {
+              TabLineSel = { bg = C.pink },
+              CmpBorder = { fg = C.surface2 },
+              Pmenu = { bg = C.none },
+              TelescopeBorder = { link = "FloatBorder" },
+            }
+          end,
+        },
+      }
+      vim.cmd [[colorscheme catppuccin]]
+    end
+  },
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    config = function()
-      require('rose-pine').setup({
-        disable_italics = true,
-        bold_vert_split = true,
-        -- disable_background = true,
-        -- disable_float_background = false,
-      })
-    end
   },
   {
     "folke/tokyonight.nvim",
@@ -28,15 +70,9 @@ return {
     opts = {
       style = "dawn",
     },
-  }
-  -- {
-  --   'uloco/bluloco.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   dependencies = { 'rktjmp/lush.nvim' },
-  --   config = function()
-  --     -- your optional config goes here, see below.
-  --     vim.cmd([[colorscheme bluloco]])
-  --   end,
-  -- },
+  },
+  {
+    'sainnhe/gruvbox-material'
+  },
+  { "EdenEast/nightfox.nvim" },
 }
