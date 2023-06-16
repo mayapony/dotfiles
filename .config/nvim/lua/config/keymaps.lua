15162acf6f9af271058b10a16ec5fe0a1f996f1d
mayapony <mayapony@foxmail.com>
2023-06-16 22:58:53 +0800

    ✨ update my config
    Former-commit-id: 9b06ec6510075e7e5b3d7323a3b9275c145d34e1

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -93,17 +93,17 @@ map("n", "<leader>uf", require("lazyvim.plugins.lsp.format").toggle, { desc = "T
 map("n", "<leader>us", function() Util.toggle("spell") end, { desc = "Toggle Spelling" })
 map("n", "<leader>uw", function() Util.toggle("wrap") end, { desc = "Toggle Word Wrap" })
 map("n", "<leader>ul", function()
-  Util.toggle("relativenumber", true)
-  Util.toggle("number")
+	Util.toggle("relativenumber", true)
+	Util.toggle("number")
 end, { desc = "Toggle Line Numbers" })
 map("n", "<leader>ud", Util.toggle_diagnostics, { desc = "Toggle Diagnostics" })
 local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
 map("n", "<leader>uc", function() Util.toggle("conceallevel", false, { 0, conceallevel }) end,
-  { desc = "Toggle Conceal" })
+	{ desc = "Toggle Conceal" })
 
 -- lazygit
 map("n", "<leader>gg", function() Util.float_term({ "lazygit" }, { cwd = Util.get_root() }) end,
-  { desc = "Lazygit (root dir)" })
+	{ desc = "Lazygit (root dir)" })
 map("n", "<leader>gG", function() Util.float_term({ "lazygit" }) end, { desc = "Lazygit (cwd)" })
 
 -- quit
@@ -111,7 +111,7 @@ map("n", "<leader>qa", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- highlights under cursor
 if vim.fn.has("nvim-0.9.0") == 1 then
-  map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+	map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 end
 
 -- floating terminal
--- .config/nvim/lua/plugins/ui/notify.lua
+++ .config/nvim/lua/plugins/ui/notify.lua
@@ -20,13 +20,4 @@ return {
       return math.floor(vim.o.columns * 0.3)
     end,
   },
-  init = function()
-    -- when noice is not enabled, install notify on VeryLazy
-    local Util = require("lazyvim.util")
-    if not Util.has("noice.nvim") then
-      Util.on_very_lazy(function()
-        vim.notify = require("notify")
-      end)
-    end
-  end,
 }
