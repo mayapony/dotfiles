d026448370aec3c5786d1a65bd76e194b9d59666
mayapony <mayapony@foxmail.com>
2025-06-14 15:53:54 +0800

    ✨ chezmoi use delta

--- dot_zsh/alias.zsh
+++ dot_zsh/alias.zsh
@@ -3,9 +3,16 @@ alias c=clear
 alias lg=lazygit
 alias vi=nvim
 
+# eza
 alias ls="eza --icons -1"
 alias ll="eza --icons -l"
 alias la="eza --icons -a"
 alias tree="exa --icons --tree"
-alias bf=chezmoi
+
 alias pac=sudo pacman
+
+# chezmoi
+alias bf=chezmoi
+alias bfd="chezmoi diff"
+alias bfa="chezmoi apply"
+alias bfc="chezmoi cd"
